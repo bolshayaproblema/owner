10bba30dbe6ab19bcfe26ae8b6b3c3b16eb9a9e6
Yura Kondratiev <yuraa.kondratiev@yandex.ru>
2023-06-30 20:15:52 +0300

    added extends

--- src/test/java/qa/guru/owner/tests/WebTest.java
+++ src/test/java/qa/guru/owner/tests/WebTest.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class WebTest {
+public class WebTest extends TestBase {
     @Test
     void githubTest() {
         open("/");
